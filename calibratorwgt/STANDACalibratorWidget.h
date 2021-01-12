749f157ae9cca57854053166dc50da1b585c4de9
satyshev <satyshev@jinr.ru>
2021-01-12 13:32:13 +0300

    add fish of the CalibratorWidget

--- calibratorwgt/STANDACalibratorWidget.h
+++ calibratorwgt/STANDACalibratorWidget.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <vector>
+
+//#include <ximc.h>
+
 #include <QWidget>
+#include <QString>
+#include <QStringList>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QLabel>
+#include <QPushButton>
 
 class STANDACalibratorWidget : public QWidget
 {
@@ -8,5 +18,44 @@ class STANDACalibratorWidget : public QWidget
 public:
     STANDACalibratorWidget(QWidget *parent = nullptr);
     ~STANDACalibratorWidget();
+
+    void LoadAvailableDevices();
+    void addDevice(QString devName) { m_devNamesList << devName; }
+
+    void InitDev(int index);
+    void DeleteDev(int index);
+
+    void setndevs(int ndevs) { m_ndevs=ndevs; }
+
+    int getndevs() { return m_ndevs; }
+
+private:
+    int m_ndevs=0;
+    int m_curIndex=0;
+    QStringList m_devNamesList;
+    std::vector<int> m_vDevVoltages;
+    std::vector<int> m_vDevSpeeds;
+    std::vector<int> m_vDevZeroPoses;
+    std::vector<int> m_vDevMaxPoses;
+
+    /*Labels*/
+    QLabel *m_pNomVoltageLabel;
+    QLabel *m_pNomSpeedLabel;
+    QLabel *m_pCurPosLabel;
+
+    /*Editors*/
+    QLineEdit *m_pNomVoltageEdit;
+    QLineEdit *m_pNomSpeedEdit;
+
+    QTextEdit *m_pInfoWindow;
+
+    /*Buttons*/
+    QPushButton *m_pSetNomVoltage;
+    QPushButton *m_pSetNomSpeed;
+    QPushButton *m_pSetZeroPos;
+    QPushButton *m_pSetMaxPos;
+    QPushButton *m_pLeft;
+    QPushButton *m_pRight;
+    QPushButton *m_pNext;
 
 };
